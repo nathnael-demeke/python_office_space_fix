f9d2374060526e970e461c31557d8bb01e682d4a
nathnael-demeke <codingwithnati@gmail.com>
2023-12-10 22:18:48 +0300

    finished trying to connect to another computer

--- client.py
+++ client.py
@@ -2,12 +2,13 @@ import socket
 import pyautogui
 
 pyautogui.FAILSAFE = False
+print(socket.gethostbyname(socket.gethostname()))
 while True:
-    print(socket.gethostbyname(socket.gethostname()))
     client = socket.socket()
-    client.connect(("localhost", 12))
+    client.connect(("192.168.1.10", 12))
     list =  client.recv(1024).decode("utf-8").split()
-    
+    # for word in list:
+    #     if word == "yesItIsClicked":
+    #         print("mouse click")
     pyautogui.moveTo(int(list[0]), int(list[1]))
-    print(list)
     client.close()
